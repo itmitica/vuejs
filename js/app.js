62a1ed7e4d866931a6d98528d0f24cd09bf9da0c
itmitica <itmitica@gmail.com>
2017-12-08 01:40:34 +0200

    material design framework: basic card

---
+++ js/app.js
@@ -0,0 +1,11 @@
+var app = new Vue({
+    el: '#vdiv',
+    data: {
+        message: 'Hello Vue.js!'
+      },
+      methods: {
+        reverseMessage: function () {
+          this.message = this.message.split('').reverse().join('')
+        }
+      }
+  })
