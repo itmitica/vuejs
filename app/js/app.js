880f36b3b55786b340b82bfe17231221aaba6500
itmitica <itmitica@gmail.com>
2017-12-08 13:52:00 +0200

    intro, w/ components

--- app/js/app.js
+++ app/js/app.js
@@ -1,11 +1,24 @@
-var app = new Vue({
-    el: '#vdiv',
+var demoIntro = new Vue({
+    el: '#demo-intro',
     data: {
-        message: 'Hello Vue.js!'
-      },
-      methods: {
+        message: 'Hello Vue.js!',
+        seen: true,
+        directives: [
+            { code: '{{ }}', text: "binding content (text interpolation)" },
+            { code: 'v-bind', text: "binding element attributes" },
+            { code: 'v-model', text: "binding input elements (two way)" },
+            { code: 'v-if', text: "binding DOM structure" },
+            { code: 'v-for', text: "binding DOM structure (iterative)" }
+        ]
+    },
+    methods: {
         reverseMessage: function () {
-          this.message = this.message.split('').reverse().join('')
+            this.message = this.message.split('').reverse().join('')
+        },
+        toggleVisibility: function () {
+            this.seen = $("#toggle-visibility").is(':checked');
         }
-      }
-  })
+    }
+})
+
+
