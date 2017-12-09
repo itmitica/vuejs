cf337c9e84fbea9b426d6bb4b088edc3a0c88d11
itmitica <itmitica@gmail.com>
2017-12-10 00:54:33 +0200

    small adjustments

--- app/js/app.js
+++ app/js/app.js
@@ -5,12 +5,12 @@ var demoIntro = new Vue({
         seen: true,
         seenColor: "",
         directives: [
-            { code: '{{ property }}', effect: "Binds document content to app 'data' state", placement: "Place as content" },
-            { code: 'v-bind:attribute="property"', effect: "Binds element attribute value to app 'data' state",  placement: "Place as prefix for attribute" },
-            { code: 'v-model="property"', effect: "Binds input element value to app 'data' state, two way", placement: "Place as custom attribute for input element" },
-            { code: 'v-if="property"', effect: "Binds DOM structure to app 'data' state", placement: "Place as custom attribute for element" },
-            { code: 'v-for="var in property"', effect: "Binds DOM structure to app 'data' state", placement: "Place as custom attribute for element" },
-            { code: 'v-on:event="method"', effect: "Binds events to app methods", placement: "Place as custom event for element" }
+            { code: '{{ property }}', effect: "Binds document content to app 'data' state.", placement: "Place as content." },
+            { code: 'v-bind:attribute="property"', effect: "Binds element attribute value to app 'data' state.",  placement: "Place as prefix for attribute." },
+            { code: 'v-model="property"', effect: "Binds input element value to app 'data' state, two way.", placement: "Place as custom attribute for input element." },
+            { code: 'v-if="property"', effect: "Binds DOM structure to app 'data' state.", placement: "Place as custom attribute for element." },
+            { code: 'v-for="var in property"', effect: "Binds DOM structure to app 'data' state.", placement: "Place as custom attribute for element." },
+            { code: 'v-on:event="method"', effect: "Binds events to app methods.", placement: "Place as custom attribute for element." }
         ]
     },
     methods: {
