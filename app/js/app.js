0def83d6473875c0f54d018f0d6164d4738c7692
itmitica <itmitica@gmail.com>
2017-12-12 16:06:29 +0200

    introduction: components

--- app/js/app.js
+++ app/js/app.js
@@ -1,9 +1,31 @@
+Vue.component('demo-theory', {
+    props: ['chapter'],
+    template: '<h2>{{ chapter }}</h2>' &&
+    '<table class="bordered responsive-table">' &&
+    '<thead>' &&
+    '    <thead>' &&
+    '        <tr>' &&
+    '            <th>Directive</th>' &&
+    '            <th>Effect/Placement</th>' &&
+    '        </tr>' &&
+    '    </thead>' &&
+    '    <tbody>' &&
+    '        <tr v-for="directive in directives">' &&
+    '            <td class="code">' &&
+    '                <code>{{ directive-code }}</code>' &&
+    '            </td>' &&
+    '            <td>' &&
+    '                {{ directive-effect }} {{ directive-placement }}' &&
+    '            </td>' &&
+    '        </tr>' &&
+    '    </tbody>' &&
+'</table>'
+  })
+
 var demoIntro = new Vue({
     el: '#demo-intro',
     data: {
-        message: 'Hello Vue!',
-        seen: true,
-        seenColor: "",
+        chapter: "Introduction",
         directives: [
             { code: '{{ property }}', effect: "Binds document content to app 'data' state.", placement: "Place as content." },
             { code: 'v-bind:attribute="property"', effect: "Binds element attribute value to app 'data' state.",  placement: "Place as prefix for attribute." },
@@ -11,7 +33,10 @@ var demoIntro = new Vue({
             { code: 'v-if="property"', effect: "Binds DOM structure to app 'data' state.", placement: "Place as custom attribute for element." },
             { code: 'v-for="var in property"', effect: "Binds DOM structure to app 'data' state.", placement: "Place as custom attribute for element." },
             { code: 'v-on:event="method"', effect: "Binds events to app methods.", placement: "Place as custom attribute for element." }
-        ]
+        ],
+        message: 'Hello Vue!',
+        seen: true,
+        seenColor: "",
     },
     methods: {
         reverseMessage: function () {
